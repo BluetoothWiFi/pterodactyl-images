da226782b8b579b6f7bb332bc50437d1dcac5dae
Daniil <67960469+BluetoothWiFi@users.noreply.github.com>
2024-08-22 16:12:29 +0300

    Update end_screen.sh

--- games/carbon/end_screen.sh
+++ games/carbon/end_screen.sh
@@ -1,12 +1,12 @@
 source /helpers/messages.sh
 
 Red "╭──────────────────────────────────────────────────╮"
-Red "│     Thats it from us! Enjoy your rust server!    │"
+Red "│     На этом всё! Наслаждайтесь ваши сервером!    │"
 Red "├──────────────────────────────────────────────────┤"
-Red "│    For More Information See The Documentation    │"
+Red "│    Больше информации можно увидеть в доках :)    │"
 Red "│        https://tinyurl.com/aiorustegg            │"
 Red "│         https://discord.gg/CUH3vADmMp            │"
 Red "╰──────────────────────────────────────────────────╯"
 
-Info "Starting your Rust server now!"
+Info "Запускаем твой сервер!"
 sleep 2
