c2b9db17a93d93b1048594306895338fe2e10280
Daniil <67960469+BluetoothWiFi@users.noreply.github.com>
2024-08-22 16:29:07 +0300

    Update app_public_ip.sh

--- games/carbon/sections/app_public_ip.sh
+++ games/carbon/sections/app_public_ip.sh
@@ -7,31 +7,31 @@ source /helpers/messages.sh
 ########################
 
 Debug "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
-Debug "Inside /sections/app_public_ip.sh file!"
+Debug "Инициализирую /sections/app_public_ip.sh!"
 
-Info "Setting App Public IP"
+Info "Настройка айпи-адреса"
 
 # Make internal Docker IP address available to processes.
-Debug "Making interal Docker IP address available to processes..."
+Debug "Предоставляю доступ другим процессам к внутреннему айпи докера..."
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
 # Grab the public IP address of the node
-Debug "Grabbing the public IP address of the node"
+Debug "Получаю публичный айпи ноды сервера"
 PUBLIC_IP=$(curl -sS ifconfig.me)
 
-Debug "Internal IP: ${INTERNAL_IP}"
-Debug "Public IP: ${PUBLIC_IP}"
+Debug "Внутренний айпи: ${INTERNAL_IP}"
+Debug "Публичный айпи: ${PUBLIC_IP}"
 
 # If there is no app public ip set then set it to the public IP address of the node
 if [ -z ${APP_PUBLIC_IP} ]; then
-    Info "Setting APP_PUBLIC_IP address to the public IP address (${PUBLIC_IP}) of the node."
+    Info "Изменяю APP_PUBLIC_IP на айпи (${PUBLIC_IP}) ноды."
     APP_PUBLIC_IP=${PUBLIC_IP}
 # Otherwise the person did set the app public IP, so warn them that this could be dangerous.
 else
-    Warn "You did not leave the APP_PUBLIC_IP variable blank. Lets hope you know what you're doing!"
+    Warn "Вы не оставляли переменную APP_PUBLIC_IP пустой.. Будем надяться, что вы знаете что делаете!"
 fi
 
 # Display what the App Public IP is set to
-Info "App Public IP set to: ${APP_PUBLIC_IP}"
+Info "Публичный айпи адрес теперь: ${APP_PUBLIC_IP}"
 
-Success "App Public IP check successful!"
+Success "Публичный айпи был успешно установлен и проверен!"
