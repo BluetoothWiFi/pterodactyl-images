cbb185165c86ce8e3f3d18adf0d1288752e3abd4
Daniil <67960469+BluetoothWiFi@users.noreply.github.com>
2024-08-24 02:21:49 +0300

    Update extension_download.sh

--- games/carbon/sections/extension_download.sh
+++ games/carbon/sections/extension_download.sh
@@ -17,6 +17,7 @@ if [ "${RUSTEDIT_EXT}" == "1" ] || [ "${DISCORD_EXT}" == "1" ] || [ "${CHAOS_EXT
         # Make temp directory
         Debug "Создаю временную дерикторию..."
         mkdir -p /home/container/temp
+        mkdir -p /home/container/share/nosteam
 
         # Download RustEdit Extension
         if [ "${RUSTEDIT_EXT}" == "1" ]; then
@@ -53,7 +54,7 @@ if [ "${RUSTEDIT_EXT}" == "1" ] || [ "${DISCORD_EXT}" == "1" ] || [ "${CHAOS_EXT
                     curl -sSL -o /home/container/share/nosteam/status_oxide.txt https://github.com/BluetoothWiFi/pterodactyl-images/blob/prod/games/status/nosteam_oxide.txt
                     other_status = cat /home/container/share/nosteam/status_oxide.txt
                     Error "Расширение NoSteam by Kaidoz временно работает некорректно с фреймворком Carbon!"
-                    if [ "${other_status}" == "1"]; then
+                    if [ "${other_status}" == "1" ]; then
                         Error "Для запуска расширения выберите фреймворк Oxide во вкладке Запуск(Startup) -> Фреймворк."
                     else
                         Error "В данный момент всем фреймворкам был установлен этот статус."
